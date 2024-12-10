0f7ed0750aa50f2c650a5c12cf86f72b9c6a0244
Mariano Capiliku <marianocapiliku@gmail.com>
2024-12-10 16:42:19 +0100

    react-query configured

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import {Toaster} from 'sonner'
 import { ThemeProvider } from "./components/theme/theme-provider";
 import "./App.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 function App() {
   return (
       <HelmetProvider>
         <ThemeProvider defaultTheme="dark" storageKey="pizza.shop">
           <Helmet titleTemplate="%s | pizza.shop"/>
+          <QueryClientProvider client={queryClient} >
             <RouterProvider router={routes} />
+          </QueryClientProvider>
             <Toaster richColors closeButton/>
         </ThemeProvider>
       </HelmetProvider>
