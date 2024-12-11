08e2b6030db0be657ed7a9b455c82fcf296433c3
Mariano Capiliku <marianocapiliku@gmail.com>
2024-12-11 12:23:07 +0100

    Created loading effect using Skeleton component from shadcnui

--- src/components/AccountMenu.tsx
+++ src/components/AccountMenu.tsx
@@ -4,17 +4,18 @@ import { Building, ChevronDown, LogOut } from "lucide-react"
 import { useQuery } from "@tanstack/react-query"
 import { getUserProfile } from "../api/getUserProfile"
 import { getManagedRestaurant } from "../api/getManagedRestaurant"
+import { Skeleton } from "./ui/skeleton"
 
 
 const AccountMenu = () => {
 
-  const {data: profile} = useQuery({
+  const {data: profile, isLoading: isuserProfileLoading} = useQuery({
     queryKey:['profile'],
     queryFn: getUserProfile,
 
   })
 
-  const {data: restaurantInfo} = useQuery(
+  const {data: restaurantInfo, isLoading: isRestaurantLoading} = useQuery(
     {
       queryKey: ['restaurant'],
       queryFn: getManagedRestaurant
@@ -24,16 +25,26 @@ const AccountMenu = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex items-center gap-2 select-none">
-          {restaurantInfo?.name}
-          <ChevronDown/>
-        </Button>
+     
+      <Button variant="outline" className="flex items-center gap-2 select-none">
+        {isRestaurantLoading ? <Skeleton className="h- w-40"/> : restaurantInfo?.name}
+        <ChevronDown/>
+   </Button>
+      
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-            <span>{profile?.name}</span>
-            <span className="text-xs font-normal text-muted-foreground ">{profile?.email}</span>
+           {isuserProfileLoading ? 
+           (<div className="space-y-1.5">
+            <Skeleton className="h-4 w-32"/>
+            <Skeleton className="h-3 w-24"/>
+           </div>) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground ">{profile?.email}</span>
+           </>)
+           }
         </DropdownMenuLabel>
 
         <DropdownMenuSeparator/>
